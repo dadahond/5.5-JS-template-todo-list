38e72acc0a5e8f2da91e7b95079d341ef32dbdd7
dadahon <dadahon@gmail.com>
2024-10-29 10:40:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const template = document.querySelector("template");
+const addNewTodo = document.getElementById("add-new-todo");
+const list = document.getElementById("list");
+const textInput = document.getElementById("text-input");
+
+function deleteTodo(e) {
+    e.closest("li").remove();
+}
+
+function doneTodo(e) {
+    e.closest("li").classList.toggle("done");
+}
+
+addNewTodo.addEventListener("click", () => {
+    const clone = template.content.cloneNode("true");
+    const p = clone.querySelector("p");
+    const done = clone.querySelector("#done");
+    const deleteBtn = clone.querySelector("#delete");
+    deleteBtn.setAttribute("onclick", `deleteTodo(this)`);
+    done.setAttribute("onclick", `doneTodo(this)`);
+    p.textContent = textInput.value;
+    list.appendChild(clone);
+
+    textInput.value = "";
+});
